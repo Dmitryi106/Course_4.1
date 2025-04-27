158b2289c83cf8b16978836a5f021a742f006ac4
Dmitryi106 <156860558+Dmitryi106@users.noreply.github.com>
2025-04-27 16:52:43 +0700

    start

--- Course_4.1/src/main.py
+++ Course_4.1/src/main.py
@@ -1,5 +1,3 @@
-import Category
-import Product
 import utils
 
 loaded = utils.load_data_from_json()
--- Course_4.1/src/utils.py
+++ Course_4.1/src/utils.py
@@ -6,7 +6,7 @@ from src.Product import Product
 
 def load_data_from_json():
     categories = []
-    with open("Product.json",'r', encoding='UTF-8') as file:
+    with open("Product.json", 'r', encoding='UTF-8') as file:
         data = json.load(file)
         for category_data in data:
             products = []
